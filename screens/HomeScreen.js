feddfa7a0ac3ac5c599596b85406f5915e5970ed
code.chhatra <rameshdiwedi022@gmail.com>
2023-12-08 15:33:07 +0530

    main

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>I am The Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
